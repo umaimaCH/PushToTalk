e023f5cae917f04e8aa8148270dde1b381a278c0
umaimaCH <66298306+umaimaCH@users.noreply.github.com>
2022-01-05 21:00:54 +0100

    Update

--- app/src/main/java/com/example/rainbowptt/ListOfBubblesActivity.java
+++ app/src/main/java/com/example/rainbowptt/ListOfBubblesActivity.java
@@ -3,6 +3,7 @@ package com.example.rainbowptt;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -37,39 +38,18 @@ import java.util.List;
 
 public class ListOfBubblesActivity extends Activity {
     ImageButton back, reload, delete,edit;
+    static String text;
     ListView bubbles;
     ListView bubbless;
     public final static String EXTRA_MESSAGE = "MESSAGE";
     public final static String EXTRA_MESSAGEE = "MESSAGEE";
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listofbubbles);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater.inflate(R.layout.listofbubbles_items, null);
         back = findViewById(R.id.imageButton2);
-        delete = v.findViewById(R.id.delete);
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView txt = v.findViewById(R.id.bubble_name);
-                String text = txt.toString().trim();
-                Room roomyy = RainbowSdk.instance().bubbles().findBubblesByName(text).get(0);
-                RainbowSdk.instance().bubbles().deleteBubble(roomyy, new IRoomProxy.IDeleteRoomListener() {
-                    @Override
-                    public void onRoomDeletedSuccess() {
-                        RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "bubble deleted");
-                    }
-
-                    @Override
-                    public void onRoomDeletedFailed() {
-                        RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "nop");
-
-                    }
-                });
-            }
-        });
-
         edit = findViewById(R.id.edit);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,7 +98,7 @@ public class ListOfBubblesActivity extends Activity {
             }
 
         }
-        List<String> name = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
         for(Room room :rooms){
             name.add(room.getName());
         }
@@ -128,7 +108,7 @@ public class ListOfBubblesActivity extends Activity {
             @Override
             public View getView(int position, View convertView, ViewGroup parent){
                 View view = super.getView(position, convertView, parent);
-                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+                TextView tv = (TextView) view.findViewById(R.id.bubble_name);
                 tv.setTextColor(Color.GREEN);
                 return view;
             }
@@ -202,9 +182,8 @@ public class ListOfBubblesActivity extends Activity {
 
             }
         });
-        ArrayAdapter<String> adapterr = new ArrayAdapter<String>(ListOfBubblesActivity.this,
-                R.layout.listofbubbles_items, R.id.bubble_name, name);
-        adapterr.notifyDataSetInvalidated();
+        ArrayAdapter<String> adapterr = new MyListViewAdapter(this, name);
+        //adapterr.notifyDataSetInvalidated();
         bubbless.setAdapter(adapterr);
         bubbless.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         bubbless.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -276,6 +255,29 @@ public class ListOfBubblesActivity extends Activity {
         });
 
 
+
+        /*delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txt = v.findViewById(R.id.bubble_name);
+                String text = txt.toString().trim();
+                Room roomyy = RainbowSdk.instance().bubbles().findBubblesByName(text).get(0);
+                RainbowSdk.instance().bubbles().deleteBubble(roomyy, new IRoomProxy.IDeleteRoomListener() {
+                    @Override
+                    public void onRoomDeletedSuccess() {
+                        RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "bubble deleted");
+                    }
+
+                    @Override
+                    public void onRoomDeletedFailed() {
+                        RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "nop");
+
+                    }
+                });
+            }
+        });*/
+
+
 /*
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -288,6 +290,42 @@ public class ListOfBubblesActivity extends Activity {
     }
     public boolean hasMicrophonePermission() {
         return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void deleteBubble(){
+        Room roomyy = RainbowSdk.instance().bubbles().findBubblesByName(MyListViewAdapter.txt).get(0);
+        RainbowSdk.instance().bubbles().deleteBubble(roomyy, new IRoomProxy.IDeleteRoomListener() {
+            @Override
+            public void onRoomDeletedSuccess() {
+                RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "bubble deleted");
+            }
+
+            @Override
+            public void onRoomDeletedFailed() {
+                RBLog.warn("LOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_TAG", "nop");
+
+            }
+        });
+
+    }
+
+    public static void editBubble() {
+        String neew = MyListViewAdapter.nameN;
+        String old = MyListViewAdapter.ancienN;
+        Room roomyy = RainbowSdk.instance().bubbles().findBubblesByName(old).get(0);
+        RainbowSdk.instance().bubbles().changeBubbleData(roomyy,neew,roomyy.getTopic(),true,true,true, roomyy.getAutoRegister(), new IRoomProxy.IChangeRoomDataListener() {
+
+            @Override
+            public void onChangeRoomDataSuccess(Room room) {
+
+            }
+
+            @Override
+            public void onChangeRoomDataFailed(String roomId) {
+
+            }
+        });
+
     }
 
 
